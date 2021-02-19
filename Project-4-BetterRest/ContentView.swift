aff692ad58ce071a1eb6336fb5b55fbdfea0fed0
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-02-19 14:58:17 +0600

    Initial Commit

---
+++ Project-4-BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-4-BetterRest
+//
+//  Created by Sheikh Bayazid on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
